01d34f805dc2b17b165e403faa6b87b2d448b440
pradeep-1995 <pradeep.ku.parida2012@gmail.com>
2026-02-25 21:19:13 +0530

    CICD run 3

--- src/models/register_model.py
+++ src/models/register_model.py
@@ -16,26 +16,26 @@ warnings.filterwarnings("ignore")
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-#dagshub_token = os.getenv("CAPSTONE_TEST")
-#if not dagshub_token:
-#    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-#os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-#os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-#dagshub_url = "https://dagshub.com"
-#repo_owner = "pradeep-1995"
-#repo_name = "Capstone_project_MLops"
+dagshub_url = "https://dagshub.com"
+repo_owner = "pradeep-1995"
+repo_name = "Capstone_project_MLops"
 
 # Set up MLflow tracking URI
-#mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri("https://dagshub.com/pradeep-1995/Capstone_project_MLops.mlflow")
-dagshub.init(repo_owner='pradeep-1995', repo_name='Capstone_project_MLops', mlflow=True)
+#mlflow.set_tracking_uri("https://dagshub.com/pradeep-1995/Capstone_project_MLops.mlflow")
+#dagshub.init(repo_owner='pradeep-1995', repo_name='Capstone_project_MLops', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 def load_model_info(file_path: str) -> dict:
